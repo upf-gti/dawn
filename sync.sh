1c9d9b8428ea226608679ff41cebfbc1376bbbb2
Mark Sibly <marksibly@gmail.com>
2024-05-17 18:12:11 +1200

    Manually align submodule commits

--- sync.sh
+++ sync.sh
@@ -2,5 +2,8 @@
 # git remote add upstream https://dawn.googlesource.com/dawn
 
 git fetch upstream && \
-git merge upstream/main && \
-git submodule update --merge
+git merge upstream/main
+
+# Careful, this moves all submodules to HEAD of their remotes, but this is not always where they are at google.
+#
+# git submodule update --remote
